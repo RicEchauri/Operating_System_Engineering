037c50afaf4ea03661002ecd8d6fff332e6887cf
Ricardo Corona Echauri <112343487+RicEchauri@users.noreply.github.com>
2023-02-16 13:06:15 -0600

    Update ls.c
    Code explained

--- user/ls.c
+++ user/ls.c
@@ -4,12 +4,14 @@
 #include "kernel/fs.h"
 
 char*
-fmtname(char *path)
+fmtname(char *path) //function that recive and return a string 
 {
-  static char buf[DIRSIZ+1];
-  char *p;
+    //DIRSIZ = 14
+  static char buf[DIRSIZ+1]; //string with size of 14+1 
+  char *p; //pointer 
 
   // Find first character after last slash.
+  //strlen()=return the size of a string 
   for(p=path+strlen(path); p >= path && *p != '/'; p--)
     ;
   p++;
@@ -17,8 +19,10 @@ fmtname(char *path)
   // Return blank-padded name.
   if(strlen(p) >= DIRSIZ)
     return p;
+    //memmove(destination,source,): copy a block of memory of a ubication to another
   memmove(buf, p, strlen(p));
-  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  //memset()= set a whole block of memory to a particular value
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));//set a value of ' ' on all the sapaces of buf that have not a value
   return buf;
 }
 
@@ -26,35 +30,40 @@ void
 ls(char *path)
 {
   char buf[512], *p;
-  int fd;
+  int fd;   //file descriptor: int value that will return open 
   struct dirent de;
   struct stat st;
 
-  if((fd = open(path, 0)) < 0){
-    fprintf(2, "ls: cannot open %s\n", path);
+  if((fd = open(path, 0)) < 0){  // if fd return a -1 value it's because fd could open the file
+    //file descriptor= 2 =stderr standar error, 
+    //fprintf(file descriptor,"%s %d\n", nombre, numero) = write a character on the file descriptor 
+    fprintf(2, "ls: cannot open %s\n", path); //rewrite de standar error to what i want
     return;
   }
-
+    //stat gives you a status information about a file 
+    //fstat works like stat but instead of use filename use a file descriptor
   if(fstat(fd, &st) < 0){
     fprintf(2, "ls: cannot stat %s\n", path);
     close(fd);
     return;
   }
+    //switc=selector whit a conditional int or char 
+  switch(st.type){ //swict depending the type of file of st 
 
-  switch(st.type){
-  case T_FILE:
+  case T_FILE: //T_FILE = 2
+    
     printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
     break;
 
-  case T_DIR:
+  case T_DIR: //T_DIR = 1
     if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
       printf("ls: path too long\n");
-      break;
+      break;                                                                                                                                                                                                                                                                                    
     }
-    strcpy(buf, path);
+    strcpy(buf, path); //copia el contenido de path al buff
     p = buf+strlen(buf);
     *p++ = '/';
-    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){ //read(); return the number of characters readed 
       if(de.inum == 0)
         continue;
       memmove(p, de.name, DIRSIZ);
