5bdf6a0cfac3ea3fe8f2b3465df8d98826b07ec7
Ricardo Corona Echauri <112343487+RicEchauri@users.noreply.github.com>
2023-02-17 10:49:32 -0600

    Update find.c

--- user/find.c
+++ user/find.c
@@ -27,7 +27,7 @@ fmtname(char *path) //function that recive and return a string
 }
 
 void
-ls(char *path)
+find (char *path , char* filename)
 {
   char buf[512], *p;
   int fd;   //file descriptor: int value that will return open 
@@ -37,58 +37,59 @@ ls(char *path)
   if((fd = open(path, 0)) < 0){  // if fd return a -1 value it's because fd could open the file
     //file descriptor= 2 =stderr standar error, 
     //fprintf(file descriptor,"%s %d\n", nombre, numero) = write a character on the file descriptor 
-    fprintf(2, "ls: cannot open %s\n", path); //rewrite de standar error to what i want
+    fprintf(2, "find: cannot open %s\n", path); //rewrite de standar error to what i want
     return;
   }
     //stat gives you a status information about a file 
     //fstat works like stat but instead of use filename use a file descriptor
   if(fstat(fd, &st) < 0){
-    fprintf(2, "ls: cannot stat %s\n", path);
+    fprintf(2, "find: cannot stat %s\n", path);
     close(fd);
     return;
   }
-    //switc=selector whit a conditional int or char 
-  switch(st.type){ //swict depending the type of file of st 
-
-  case T_FILE: //T_FILE = 2
-    
-    printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
-    break;
-
-  case T_DIR: //T_DIR = 1
-    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
-      printf("ls: path too long\n");
-      break;                                                                                                                                                                                                                                                                                    
-    }
-    strcpy(buf, path); //copia el contenido de path al buff
-    p = buf+strlen(buf);
-    *p++ = '/';
-    while(read(fd, &de, sizeof(de)) == sizeof(de)){ //read(); return the number of characters readed 
-      if(de.inum == 0)
-        continue;
-      memmove(p, de.name, DIRSIZ);
-      p[DIRSIZ] = 0;
-      if(stat(buf, &st) < 0){
-        printf("ls: cannot stat %s\n", buf);
-        continue;
-      }
-      printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
-    }
-    break;
+  
+  if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+    printf("find: path too long\n");
+    break;                                                                                                                                                                                                                                                                                    
   }
+  strcpy(buf, path); //copia el contenido de path al buff
+  p = buf+strlen(buf);
+  *p++ = '/';
+  while(read(fd, &de, sizeof(de)) == sizeof(de)){ //read(); return the number of characters readed 
+    if(de.inum == 0)
+      continue;
+    if(strcmp(de.name,".")==0 || strcmp(de.name, "..")==0)
+    {
+      continue;
+    }
+    memmove(p, de.name, DIRSIZ);
+    p[DIRSIZ] = 0;
+    if(stat(buf, &st) < 0){
+      printf("ls: cannot stat %s\n", buf);
+      continue;
+    }
+  //switc=selector whit a conditional int or char 
+    switch(st.type){ //swict depending the type of file of st  
+      case T_FILE:
+        if(strcmp(de.name, filename)==0)
+        {
+          printf("%s\n",buf);
+        }
+        break;
+      case T_DIR:
+        find(buf,filename);
+    }
+  }
+
   close(fd);
 }
 
 int
-main(int argc, char *argv[])
+main(int argc, char **argv)
 {
-  int i;
-
-  if(argc < 2){
-    ls(".");
-    exit(0);
+  if(argc < 3){
+    exit(1);
   }
-  for(i=1; i<argc; i++)
-    ls(argv[i]);
+  find(argv[1],argv[2]);
   exit(0);
 }
